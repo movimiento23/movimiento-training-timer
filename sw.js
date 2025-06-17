05c6fda465204821c38c7131e7592bb812df13b6
movimiento23 <josemancillacardozo@gmail.com>
2025-06-17 11:35:48 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,91 +1,27 @@
-const CACHE_NAME = 'movimiento-training-v1';
-const URLS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/script.js',
-  '/style.css',
-  '/manifest.json',
-  '/banda de suspension.jpg',
-  '/wallpaper.wiki-Wallpapers-Free-Crossfit-Download-PIC-WPB004205-1024x683 (1).jpg',
-  '/logo.png',
-  '/icon-192.png',
-  '/icon-512.png',
-  '/start_work.mp3',
-  '/start_rest.mp3',
-  '/countdown.mp3',
-  '/finish.mp3',
-  'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Roboto+Mono:wght@700&display=swap',
-  'https://fonts.gstatic.com/s/roboto/v30/KFOlCnqEu92Fr1MmEU9fBBc4.woff2',
-  'https://fonts.gstatic.com/s/roboto/v30/KFOlCnqEu92Fr1MmWUlfBBc4.woff2',
-  'https://fonts.gstatic.com/s/roboto/v30/KFOlCnqEu92Fr1MmYUtfBBc4.woff2',
-  'https://fonts.gstatic.com/s/robotomono/v23/L0xuDF4xlVMF-BfR8bXMIhJHg45mwgGEFl0_3vrtSM1J-g.woff2'
-];
-
-// Install the service worker and cache all assets
 self.addEventListener('install', event => {
+  console.log('✅ Service Worker instalado');
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(URLS_TO_CACHE);
-      })
+    caches.open('movimiento-training-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/script.js',
+        '/style.css',
+        '/icon-192x192.png',
+        '/icon-512x512.png',
+        '/countdown.mp3',
+        '/finish.mp3',
+        '/start_rest.mp3',
+        '/start_work.mp3'
+      ]);
+    })
   );
 });
 
-// Fetch assets from cache or network
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // Clone the request because it's a one-time use stream
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          response => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              if (response.type === 'opaque' && response.url.startsWith('https://fonts.gstatic.com')) {
-                 // allow opaque responses for google fonts
-              } else {
-                return response;
-              }
-            }
-            
-            // Clone the response because it's also a one-time use stream
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                // We only cache GET requests
-                if(event.request.method === 'GET') {
-                    cache.put(event.request, responseToCache);
-                }
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-});
-
-// Clean up old caches
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
