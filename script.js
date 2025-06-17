5611de0aa5b575513ee9271c87dbc742bba658da
movimiento23 <josemancillacardozo@gmail.com>
2025-06-17 11:52:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // --- PWA Service Worker Registration ---
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/service-worker.js')
       .then(registration => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
